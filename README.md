f8fb0d60902925c1b8d385c44624ac1506ac4e99
Armand Niampa <armand.niampa@gmail.com>
2016-01-18 23:25:36 +0100

    Add nginx configuration.

---
+++ README.md
@@ -0,0 +1,2 @@
+sed -i -e "s/chaines1/chaine2/g" fichier
+sed -i -e "/$v1/$v2/g" nginx.host
---
+++ default.sh
@@ -0,0 +1,5 @@
+apt-get install language-pack-fr -y
+sudo apt-get install curl
+
+adduser deploy #--disabled-password
+adduser deploy sudo
