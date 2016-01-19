f3e9e7fd6661baa4fe42d2ed7bff767d10e50293
Armand Niampa <armand.niampa@gmail.com>
2016-01-19 01:05:49 +0100

    Default script.

--- README.md
+++ README.md
@@ -1,6 +1,31 @@
 # vps
 Setup Vps with Unicorn - Nginx - PostgreSql - Ruby On Rails - Mina
 
+# vps_setup
+apt-get install language-pack-fr -y
+apt-get install curl
+
+dpkg-reconfigure tzdata
+
+adduser rails --disabled-password
+adduser rails sudo
+sudo chown rails /srv/
+
+mkdir /home/rails/.ssh
+cp ~/.ssh/authorized_keys /home/rails/.ssh/
+chown rails.rails /home/rails/.ssh -R
+chmod go-rwx /home/rails/.ssh -R
+
+echo '\nrails ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+
+rails ALL=(ALL) NOPASSWD: ALL
+
+# passwd -l rails
+ssh-keygen
+ssh-copy-id rails@46.101.150.159
+
+netstat --listening --tcp
+ufw allow 22 && ufw logging off && ufw enable && ufw status
 
 sed -i -e "s/chaines1/chaine2/g" fichier
 sed -i -e "/$v1/$v2/g" nginx.host
--- default.sh
+++ default.sh
@@ -1,5 +1,53 @@
-apt-get install language-pack-fr -y
-sudo apt-get install curl
+#!/bin/bash
+echo "================== Reconfigure time zone ============"
+  dpkg-reconfigure tzdata
+echo ""
 
-adduser deploy #--disabled-password
-adduser deploy sudo
+echo "================== Update all packages: ==============="
+  apt-get update -y && apt-get upgrade -y &&  apt-get autoremove -y
+  apt-get install language-pack-fr -y
+echo ""
+
+echo "================== Install curl git nodejs ==========="
+  apt-get -y install curl python-software-properties git-core nodejs
+echo ""
+
+echo "================== Disable SSH password authentication ==============="
+  echo -e "PasswordAuthentication no" >> /etc/ssh/sshd_config
+  sed -i -e "s/UsePAM yes/UsePAM no/g" /etc/ssh/sshd_config
+echo ""
+
+echo "================== Generate missing SSH host keys ==============="
+  /etc/init.d/ssh reload
+  ssh-keygen -A
+echo ""
+
+echo "================== Check the open portls (should be only SSH): ==============="
+  netstat --listening --tcp
+echo ""
+
+echo "================== Enable the Ubuntu firewall so that unconfigured services are not be exposed: ==============="
+  ufw allow 22 && ufw logging off && ufw enable && ufw status
+echo ""
+
+echo "================== Create User Rails ==============="
+  adduser deploy --disabled-password
+  chown rails /srv/
+echo ""
+
+echo "================== Grant User Rails sudo ==============="
+  adduser deploy sudo
+  echo -e "\nrails ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+echo ""
+
+echo "================== Copy authorized_keys ==============="
+  mkdir /home/rails/.ssh
+  cp ~/.ssh/authorized_keys /home/rails/.ssh/
+  chown rails.rails /home/rails/.ssh -R
+  chmod go-rwx /home/rails/.ssh -R
+echo ""
+
+echo "================== Reboot ==============="
+  echo 'After reboot log with rails user'
+  reboot
+echo ""
