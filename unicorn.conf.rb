229eaf7323c85fd1635e19d7ab54cb4475c99569
Armand Niampa <ayann@users.noreply.github.com>
2016-01-20 23:52:02 +0100

    Preload option.

--- unicorn.conf.rb
+++ unicorn.conf.rb
@@ -10,3 +10,22 @@ working_directory File.join(app_path, 'current')
 pid File.join(app_path, 'shared/unicorn.pid')
 stderr_path File.join(app_path, 'current/log/unicorn.log')
 stdout_path File.join(app_path, 'current/log/unicorn.log')
+
+# Load the app up before forking.
+preload_app true
+
+# Garbage collection settings.
+GC.respond_to?(:copy_on_write_friendly=) &&
+  GC.copy_on_write_friendly = true
+
+# If using ActiveRecord, disconnect (from the database) before forking.
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.connection.disconnect!
+end
+
+# After forking, restore your ActiveRecord connection.
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.establish_connection
+end
